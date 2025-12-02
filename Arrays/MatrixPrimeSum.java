69340550041f478596da4477499cfc75c4ac95ca
ArjunP <115101452+Ayy-Pee@users.noreply.github.com>
2025-12-02 20:57:44 +0530

    Update MatrixPrimeSum.java

--- Arrays/MatrixPrimeSum.java
+++ Arrays/MatrixPrimeSum.java
@@ -11,79 +11,88 @@ Row with max prime-sum: 0 (sum= 10)
 Column with max prime-sum: 1 (sum= 15)
 */
 import java.util.Scanner;
-class MatrixPrimeSum{
-    int M,N, A[][];
-    static Scanner sc = new Scanner(System.in);
-
-    MatrixPrimeSum(int mm, int nn){
+class MatrixPrimeSum {
+    int M, N;
+    int A[][];
+    MatrixPrimeSum(int mm, int nn) {
         M = mm;
         N = nn;
         A = new int[M][N];
     }
-
-    void fillarray(){
-        System.out.print("Array Elements: ");
-        for(int i = 0; i < M; i++){
-            for(int j = 0; j < N; j++){
+    void fillarray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Array elements:");
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
                 A[i][j] = sc.nextInt();
+                if (A[i][j] < 0) {
+                    System.out.println("INVALID INPUT");
+                    System.exit(0);
+                }
             }
         }
     }
-
-    boolean isPrime(int num){
-        boolean flag = true;
-        if(num == 1 || num == 0)
-            flag = false;
-        else
-            for(int i = 2; i <= num / 2; i++)
-                if(num % i == 0)
-                    flag = false;
-        return flag;
-    }
-
-    void maxSum(){
-        int maxcol = 0, colpos = 0, maxrow = 0, rowpos = 0;
-
-        for(int i = 0; i < M; i++){
-            int colsum = 0, rowsum = 0;
-            for(int j = 0; j < N; j++){
-                if(isPrime(A[j][i]))
-                    colsum += A[j][i];
-                if(isPrime(A[i][j]))
-                    rowsum += A[i][j];
-            }
-            if(colsum > maxcol){
-                maxcol = colsum; colpos = i;
-            }
-            if(rowsum > maxrow){
-                maxrow = rowsum; colpos = i;
-            }
+    boolean isPrime(int num) {
+        int f = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0)
+                f++;
         }
-        System.out.println("Row with max prime-sum: " + rowpos + "(sum= " + maxrow + ")");
-        System.out.println("Column with max prime-sum: " + colpos + "(sum= " + maxcol + ")");
+        return f == 2;
     }
-
-    void displayArray(){
-        for (int[] i : A) {
-            for (int j : i) {
-                System.out.print(j + " ");
+    void maxSum() {
+        int maxRow = 0, maxCol = 0;
+        String rowIndex = "", colIndex = "";
+        System.out.println("Original array:");
+        for (int i = 0; i < M; i++) {
+            int sum = 0;
+            for (int j = 0; j < N; j++) {
+                System.out.print(A[i][j] + "\t");
+                if (isPrime(A[i][j]))
+                    sum += A[i][j];
             }
             System.out.println();
+            if (sum > maxRow) {
+                maxRow = sum;
+                rowIndex = i + "";
+            } else if (sum == maxRow) {
+                rowIndex += " " + i;
+            }
         }
+        if (maxRow == 0)
+            System.out.println("NO PRIME NUMBERS FOUND IN ANY ROW");
+        else
+            System.out.println("Row with max prime-sum: " + rowIndex + " (sum = " + maxRow + ")");
+        for (int j = 0; j < N; j++) {
+            int sum = 0;
+            for (int i = 0; i < M; i++) {
+                if (isPrime(A[i][j]))
+                    sum += A[i][j];
+            }
+            if (sum > maxCol) {
+                maxCol = sum;
+                colIndex = j + "";
+            } else if (sum == maxCol) {
+                colIndex += " " + j;
+            }
+        }
+        if (maxCol == 0)
+            System.out.println("NO PRIME NUMBERS FOUND IN ANY COLUMN");
+        else
+            System.out.println("Column with max prime-sum: " + colIndex + " (sum = " + maxCol + ")");
     }
-    public static void main(String[]args){
-        System.out.print("M: ");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("M = ");
         int M = sc.nextInt();
-        System.out.print("N: ");
+        System.out.print("N = ");
         int N = sc.nextInt();
-        if(M < 2 || N < 2 || M > 10 || N > 10){
+        if (M < 3 || M > 9 || N < 3 || N > 9) {
             System.out.println("INVALID INPUT");
             return;
         }
-        MatrixPrimeSum ob = new MatrixPrimeSum(M,N);
+        MatrixPrimeSum ob = new MatrixPrimeSum(M, N);
         ob.fillarray();
-        System.out.println("Matrix:");
-        ob.displayArray();
         ob.maxSum();
     }
 }
